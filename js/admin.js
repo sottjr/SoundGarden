9c92f72790e22cef42af62db6ef6a3cb63fa0973
Junior Sott <57429275+sottjr@users.noreply.github.com>
2022-03-27 13:51:07 +0200

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,5 @@
+//painel admin
+//botões
+let btReserves = document.querySelectorAll(".btn-dark")
+let btEdit = document.querySelectorAll(".btn-secondary")
+let btDelete = document.querySelectorAll("btn-danger")
