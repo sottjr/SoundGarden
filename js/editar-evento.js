5f290fe7bf92eb67eb2957699be8b563e0aef68a
Junior Sott <sottjr@gmail.com>
2022-03-28 19:07:22 +0200

    pull

---
+++ js/editar-evento.js
@@ -0,0 +1,16 @@
+const ID_ATUAL = window.location.href.split('=')[1]
+
+
+
+
+
+
+const inName = document.querySelector("#nome");
+// const inAtracoes = document.querySelector("#atracoes");
+// const inDescripition = document.querySelector("#descricao");
+// const inDate = document.querySelector("#data");
+// const inLotacao = document.querySelector("#lotacao");
+const inLinkImg = document.querySelector("#banner")
+// const btSend = document.querySelector("#btSend");
+// const form = document.querySelector("form")
+
