9c98d136f00c83cc90d591b16e66e801dddc202d
Junior Sott <sottjr@gmail.com>
2022-03-30 13:59:45 +0200

    ALTERAÇÃO JR

--- js/admin.js
+++ js/admin.js
@@ -4,21 +4,21 @@ const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com"
 let listEvents = async () => {
 
     //Requisição
-    const answer = await fetch(`${BASE_URL}/events`, {method: "GET", redirect: 'follow'})
+    const answer = await fetch(`${BASE_URL}/events`, { method: "GET", redirect: 'follow' })
 
     //extraindo json
     const contentAnswer = await answer.json()
-// console.log(contentAnswer)
-    contentAnswer.forEach((item,index) => {
-        table.innerHTML+= 
-        `
+    // console.log(contentAnswer)
+    contentAnswer.forEach((item, index) => {
+        table.innerHTML +=
+            `
         <tr>
-                                <th scope="row">${index+1}</th>
+                                <th scope="row">${index + 1}</th>
                                 <td id="remove">${item.scheduled}</td>
                                 <td>${item.name}</td>
                                 <td id="remove">${item.attractions}</td>
                                 <td>
-                                    <a href="reservas.html?id=${item._id}" class="btn btn-dark">ver reservas</a>
+                                    <a href="ver-reserva.html?id=${item._id}" class="btn btn-dark">ver reservas</a>
                                     <a href="editar-evento.html?id=${item._id}" class="btn btn-secondary">editar</a>
                                     <a href="excluir-evento.html?id=${item._id}" class="btn btn-danger">excluir</a>
                                 </td>
@@ -26,7 +26,7 @@ let listEvents = async () => {
         
         `
     });
-    
+
 
 };
 
