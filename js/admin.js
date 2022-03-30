930b48d7f92ce6404841f5a12cb666fc895ebf3b
Adryel Simon <adryel@live.it>
2022-03-30 11:39:44 -0300

    30/03 bugs

--- js/admin.js
+++ js/admin.js
@@ -18,19 +18,13 @@ let listEvents = async () => {
                                 <td>${item.name}</td>
                                 <td id="remove">${item.attractions}</td>
                                 <td>
-<<<<<<< HEAD
                                     <a href="reserva.html?id=${item._id}" class="btn btn-dark w-100">ver reserva</a>
-=======
-<<<<<<< HEAD
                                     <a href="ver-reserva.html?id=${item._id}" class="btn btn-dark">ver reservas</a>
                                     <a href="editar-evento.html?id=${item._id}" class="btn btn-secondary">editar</a>
                                     <a href="excluir-evento.html?id=${item._id}" class="btn btn-danger">excluir</a>
-=======
                                     <a href="reservas.html?id=${item._id}" class="btn btn-dark w-100">ver reservas</a>
->>>>>>> a8f2cf370c99ba449152b318b27ff4af49e210f2
                                     <a href="editar-evento.html?id=${item._id}" class="btn btn-secondary w-100">editar</a>
                                     <a href="excluir-evento.html?id=${item._id}" class="btn btn-danger w-100">excluir</a>
->>>>>>> 7f9dc779b690f78fa2b00393d92a9f4bb1c8d885
                                 </td>
                             </tr>
         
