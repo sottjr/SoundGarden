a7f661d331e22ad0171aeb6e568ac21ef59c8556
Adryel Simon <adryel@live.it>
2022-03-31 14:52:15 -0300

    html final

--- js/cards-events.js
+++ js/cards-events.js
@@ -13,13 +13,13 @@ body.onload = async () => {
     for (let i = 0; i < 3; i++) {
         divEventos.innerHTML += `
        
-        <article  class="bg-dark text-light bg-gradient evento card d-flex justify-content-between align-content-center p-5 m-3" style="min-height: 666px; border-radius:20px">
+        <article  class="bg-dark text-light bg-gradient evento card d-flex justify-content-between align-content-center p-5 m-3" style="min-height: 500px; border-radius:20px;">
         <div>    
-        <h2 id="nomeData">${conteudoResposta[i].name} - ${conteudoResposta[i].scheduled}</h2><hr>
-            <h4 id="atracoes" class="removeAtracao">${conteudoResposta[i].attractions}</h4>
+        <h2 id="nomeData">${conteudoResposta[i].name}</h2><hr>
+            <h4 id="atracoes" class="">${conteudoResposta[i].attractions}</h4>
             <p id="descricao" style="text-overflow: ellipsis;">${conteudoResposta[i].description}</p>
             </div>  
-               <button type="button" class="btn btn-primary bg-light text-dark border-dark" data-bs-toggle="modal"
+               <button type="button" class="hover btn btn-primary bg-light text-dark border-dark" data-bs-toggle="modal"
                 data-bs-target="#exampleModal">
                 reservar ingresso
             </button>
